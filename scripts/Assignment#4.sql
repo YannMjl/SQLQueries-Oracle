5f3e4b19f0741c3e50129ac083b58c28398707a5
Yann Mulonda <yannmjl@gmail.com>
2019-04-11 12:07:53 -0500

    question 3

--- scripts/Assignment#4.sql
+++ scripts/Assignment#4.sql
@@ -8,3 +8,9 @@ Where city = 'Seattle';
 -- 2.  list all cities where customers live. Make sure the result does not include duplicates
 Select Distinct city
 From Customer;
+
+-- 3. list the first and last name of all employees who have the last name
+-- of Stuart or the first name of John.
+Select Firstname, LastName
+From employee
+Where LastName = 'Stuart' or FirstName = 'John';
