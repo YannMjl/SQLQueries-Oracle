33267f8645208700c43ccc837c034466da20c699
Yann Mulonda <yannmjl@gmail.com>
2019-05-22 13:19:56 -0500

    added exhibits script file

---
+++ scripts/final-Exhibit-script.sql
@@ -0,0 +1,39 @@
+-- ************************************************************************************************
+-- Valencia Construction - SQL Part 2                                                             *
+-- Exhibits script                                                                                *
+-- Author : Christy Ehlert-Mackie, Yann Mulonda, Sara Spring, Ethel Osuji                         *
+-- Date : May 22, 2019                                                                            *
+-- ************************************************************************************************
+
+-- Exhibit B, Part C (equivalent to Exhibit C, Part A; Exhibit D, Part B)
+
+
+-- Exhibit B, Part D
+
+
+-- Exhibit C, Part C
+
+
+-- Exhibit D, Part D
+
+
+
+-- Exhibit D, Part E
+
+
+
+-- Exhibit D, Part F
+
+
+
+-- Exhibit E
+
+-- ************************************************************************************************
+-- (You may ignore the part of Exhibit F showing Gross, Deductions, Check#, etc.)                 *
+-- ************************************************************************************************
+-- Exhibit F, Regular Pay
+
+
+-- Exhibit F, Overtime Pay
+
+-- ************************************************************************************************
